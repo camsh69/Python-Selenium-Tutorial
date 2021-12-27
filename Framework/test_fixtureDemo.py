35d9a60efdcefd9c55e6201c6c266c4b6b42cef9
Campbell Forsyth <camsh69@gmail.com>
2021-12-27 13:32:20 +0000

    Framework tutorial

--- Framework/test_fixtureDemo.py
+++ Framework/test_fixtureDemo.py
@@ -1,6 +1,7 @@
 import pytest
 # see conftest file
 # fixtures are used for setup and tear down for test cases - conftest file to generalize and make fixture available in all files
+# when you define fixture scope as class only, it will run once before class is initiated and again at end
 
 
 @pytest.mark.usefixtures("setup")
